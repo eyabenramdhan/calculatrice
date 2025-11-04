a7881018546967e5ca85406cf5bda40181af1c0c
eya ben ramdhan <eya.benramdhan@enis.tn>
2025-11-04 22:12:36 +0100

    ajout soustraction et addition

--- Operation.java
+++ Operation.java
@@ -2,4 +2,11 @@ public class Operation{
 public static int multiplication (int a, int b){
 return(a*b);
 }
+public static int soustraction (int a, int b){
+return(a-b);
+}
+public static int addition (int a, int b){
+return(a+b);
+}
+
 }
